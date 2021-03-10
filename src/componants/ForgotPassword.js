8f1b8be9a457937e6fb1b7164b58a3a790d731f0
umesh1495 <umeshsharma1495@gmail.com>
2021-03-10 15:40:03 +0530

    first commit

---
+++ src/componants/ForgotPassword.js
@@ -0,0 +1,50 @@
+import React, { useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from "../contexts/AuthContext";
+import {Link, useHistory } from "react-router-dom";
+
+export default function ForgotPassword() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { resetPassword } = useAuth();
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+    
+        try{
+            setMessage('')
+            setError('');
+            setLoading(true);
+            await resetPassword(emailRef.current.value);
+            setMessage('Check your inbox for further instruction')
+        } catch{
+            setError('Failed to reset password');
+        }
+        setLoading(false);
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Password Reset</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    {message && <Alert variant="success">{message}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
+                        <Button disabled={loading} className="w-100" type="submit">Reset Password</Button>
+                        <div className="w-100 text-center mt-3"><Link to="/login">Log In</Link></div>
+                    </Form>
+                </Card.Body>
+            </Card> 
+            <div className="w-100 text-center mt-2">Need an account? <Link to="/signup">Sign Up</Link></div>
+        </>
+    )
+}
+
